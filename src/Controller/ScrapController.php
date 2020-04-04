0940805bba4521268cbb20bd9747342640b7176a
Tomas <kleinauskastom@gmail.com>
2020-04-04 15:24:42 +0300

    Saving images to different file

--- src/Controller/ScrapController.php
+++ src/Controller/ScrapController.php
@@ -26,58 +26,62 @@ class ScrapController extends AbstractController
      */
     public function index(Request $request)
     {
-		$client = new \GuzzleHttp\Client();
-		$response = $client->request('GET', 'https://truckmanualshub.com/category/trucks/');
-		$html= ''.$response->getBody();
-		$crawler = new Crawler($html);
-		$nodeValues = $crawler->filter('.pagination > ul > li')->each(function (Crawler $node, $i) {
-				
-			$pages=$node->text();
-		
-			$data = new \GuzzleHttp\Client();
-			$page = $data->request("GET", "https://truckmanualshub.com/category/trucks/page/$pages");
-			$codes= ''.$page->getBody();
-			$crawler = new Crawler($codes);
-			$nodeValues = $crawler->filter('#content > article')->each(function (Crawler $nodes, $i) {
-					
-			$titles = $nodes->filter('h2')->text();
-			$titles = trim($titles );
-			$images = $nodes->filter('img')->attr('src');
-			$descriptions = $nodes->filter('p')->text();
-			$descriptions =str_replace(" Read More »", "", $descriptions);
-			$categories = $nodes->filter('.categories')->text();
-			$data = [
-				'title' => $titles,
-				'description' => $descriptions,
-				'image' => $images,
-				'category' => $categories];
-			
-			return $data;			
-			});				
-		foreach ($nodeValues as $data) {
-			$repository = $this->getDoctrine()->getRepository(Scraper::class);
-			$pavadinimas = $data['title'];
-			$aprasymas = $data['description'];
-			
-			$product = $repository->findOneBy([
-				'title' => $pavadinimas,
-			]);	
-			if(!$product) 
-			{
-	
-				$em = $this->getDoctrine()->getManager();		
-				$database = new Scraper();
-				$database->setTitle($data['title']); //Respective entity methods
-				$database->setDescription($data['description']); //Respective entity methods
-				$database->setPhoto($data['image']); //Respective entity methods
-				$database->setCategory($data['category']); //Respective entity methods
-				$em->persist($database);
 
-			}	
-		}
-		if(!$product){$em->flush();}
-		});
-		echo "Duomenys perkelti į DB<br>";
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://truckmanualshub.com/category/trucks/');
+        $html= ''.$response->getBody();
+        $crawler = new Crawler($html);
+        $nodeValues = $crawler->filter('.pagination > ul > li')->each(function (Crawler $node, $i) {
+                
+            $pages=$node->text();
+        
+            $data = new \GuzzleHttp\Client();
+            $page = $data->request("GET", "https://truckmanualshub.com/category/trucks/page/$pages");
+            $codes= ''.$page->getBody();
+            $crawler = new Crawler($codes);
+            $nodeValues = $crawler->filter('#content > article')->each(function (Crawler $nodes, $i) {
+                    
+            $titles = $nodes->filter('h2')->text();
+            $titles = trim($titles );
+            $images = $nodes->filter('img')->attr('src');
+            $descriptions = $nodes->filter('p')->text();
+            $descriptions =str_replace(" Read More »", "", $descriptions);
+            $categories = $nodes->filter('.categories')->text();
+            $data = [
+                'title' => $titles,
+                'description' => $descriptions,
+                'image' => $images,
+                'category' => $categories];
+            
+            return $data;            
+            });                
+            foreach ($nodeValues as $data) {
+                $repository = $this->getDoctrine()->getRepository(Scraper::class);
+                $pavadinimas = $data['title'];
+                
+                $product = $repository->findOneBy([
+                    'title' => $pavadinimas,
+                ]);    
+                if(!$product) 
+                {
+                    $image_file = file_get_contents($data['image']);
+                    $image_name = explode("/",$data['image']);
+                    $img_name=$image_name[count($image_name)-1];
+                    file_put_contents("images/$img_name", $image_file);
+                    $em = $this->getDoctrine()->getManager();        
+                    $database = new Scraper();
+                    $database->setTitle($data['title']); //Respective entity methods
+                    $database->setDescription($data['description']); //Respective entity methods
+                    //$database->setPhoto($data['image']); //Respective entity methods
+                    $database->setPhoto("images/$img_name"); //Respective entity methods
+                    $database->setCategory($data['category']); //Respective entity methods
+                    $em->persist($database);
+
+                }    
+            }
+        if(!$product){$em->flush();}
+        });
+        echo "Duomenys perkelti į DB<br>";
         return $this->render('scrap.html.twig', []);
     }
 }
